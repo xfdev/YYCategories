7090c2746568b216c8e23cca68acbd749d887921
xfdev <anangcocoy@gmail.com>
2020-09-18 12:11:50 +0800

    关闭frame快捷方式，和masonry冲突

--- YYCategories/UIKit/UIView+YYAdd.h
+++ YYCategories/UIKit/UIView+YYAdd.h
@@ -100,17 +100,17 @@ NS_ASSUME_NONNULL_BEGIN
  */
 - (CGRect)convertRect:(CGRect)rect fromViewOrWindow:(nullable UIView *)view;
 
-
-@property (nonatomic) CGFloat left;        ///< Shortcut for frame.origin.x.
-@property (nonatomic) CGFloat top;         ///< Shortcut for frame.origin.y
-@property (nonatomic) CGFloat right;       ///< Shortcut for frame.origin.x + frame.size.width
-@property (nonatomic) CGFloat bottom;      ///< Shortcut for frame.origin.y + frame.size.height
-@property (nonatomic) CGFloat width;       ///< Shortcut for frame.size.width.
-@property (nonatomic) CGFloat height;      ///< Shortcut for frame.size.height.
-@property (nonatomic) CGFloat centerX;     ///< Shortcut for center.x
-@property (nonatomic) CGFloat centerY;     ///< Shortcut for center.y
-@property (nonatomic) CGPoint origin;      ///< Shortcut for frame.origin.
-@property (nonatomic) CGSize  size;        ///< Shortcut for frame.size.
+// 和 Masonry 冲突，因此注销
+//@property (nonatomic) CGFloat left;        ///< Shortcut for frame.origin.x.
+//@property (nonatomic) CGFloat top;         ///< Shortcut for frame.origin.y
+//@property (nonatomic) CGFloat right;       ///< Shortcut for frame.origin.x + frame.size.width
+//@property (nonatomic) CGFloat bottom;      ///< Shortcut for frame.origin.y + frame.size.height
+//@property (nonatomic) CGFloat width;       ///< Shortcut for frame.size.width.
+//@property (nonatomic) CGFloat height;      ///< Shortcut for frame.size.height.
+//@property (nonatomic) CGFloat centerX;     ///< Shortcut for center.x
+//@property (nonatomic) CGFloat centerY;     ///< Shortcut for center.y
+//@property (nonatomic) CGPoint origin;      ///< Shortcut for frame.origin.
+//@property (nonatomic) CGSize  size;        ///< Shortcut for frame.size.
 
 @end
 
