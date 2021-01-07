16ab06f7c08bcf9f42c908f274d107efd8d5f0f4
xfdev <anangcocoy@gmail.com>
2021-01-07 17:50:30 +0800

    1.0.8 提交

--- YYCategories/Foundation/NSString+FRAdd.h
+++ YYCategories/Foundation/NSString+FRAdd.h
@@ -13,7 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface NSString (FRAdd)
 
 // 格式校验
-#warning 使用 NSDataDetector 来优化
+//#warning 使用 NSDataDetector 来优化
 - (BOOL)isEmail;
 - (BOOL)isURL;
 - (BOOL)isPhoneNumber;
